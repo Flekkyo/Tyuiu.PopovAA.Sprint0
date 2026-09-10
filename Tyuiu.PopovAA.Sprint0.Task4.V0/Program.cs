9772874cc927b89c527318d8e6c551cffb5a00d1
Flekkyo <artm_popov_2008@bk.ru>
2026-09-10 22:10:39 +0500

    Task4

---
+++ Tyuiu.PopovAA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PopovAA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.PopovAA.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(100, 50));
+
+            Console.WriteLine(DataService.Multiplication(10, 50));
+
+            Console.WriteLine(DataService.Division(100, 5));
+        }
+    }
+}
