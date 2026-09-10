965f3833dfcbea47c26902db3a12ccca88d9cc43
Flekkyo <artm_popov_2008@bk.ru>
2026-09-10 21:13:13 +0500

    Task3

---
+++ Tyuiu.PopovAA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.PopovAA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.PopovAA.Sprint0.Task3.V0.Test
+{
+    public class DataServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.PopovAA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.PopovAA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.PopovAA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+        }
+    }
+}
